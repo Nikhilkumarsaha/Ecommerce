209cf5ab5f175114e2eadc71b4e7a00768cf3d84
Nikhil kumar saha <nikhilsaha92236@gmail.com>
2025-01-30 12:53:04 +0530

    first commit

---
+++ src/Component/AdminDashboard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "../Style/AdminDashboard.css"
+
+export default function AdminDashboard() {
+  return (
+    <div className='AdminDashBoard'>
+      <h1>Admin Dash Board</h1>
+    </div>
+  )
+}
